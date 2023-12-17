7cfcfd1882f58b0e69aa9d13444d8d54beeb2f5d
antoniotorres02 <torresbordonantonio@gmail.com>
2023-12-17 20:13:25 +0100

    Corregidos errores

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -7,32 +7,62 @@ const int echoPin = 3;
 float maxHeight = 0;
 float minHeight = 0;
 
+// Array de 10 muestras para el promedio
+float samples[10];
+int sampleIndex = 0;
+
+float distance = 0;
+float lastDistance = 0;
+
 // Crea una instancia del sensor
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+
 
 void setup()
 {
     // Inicia la comunicación serial
     Serial.begin(9600);
-    maxHeight = distanceSensor.measureDistanceCm();
-    minHeight = maxHeight;
+
+    distance = distanceSensor.measureDistanceCm();
+    if (distance < 1) {distance = 1;}
+    maxHeight = distance;
+    minHeight = distance;
+
+    // Inicializa el array de muestras
+    for (int i = 0; i < 10; i++)
+    {
+        distance = distanceSensor.measureDistanceCm();
+        if (distance < 1) {distance = 1;}
+        samples[i] = distance;
+        delay(10);
+    }
 
 }
 
 void loop()
 {
     // Lee la distancia en centímetros
-    float distance = distanceSensor.measureDistanceCm();
+    lastDistance = distance;
+    distance = distanceSensor.measureDistanceCm();
+    if (distance < 1) {distance = 1;}
+
+    if (distance > lastDistance + 10) {distance = lastDistance;} // Evita cambios bruscos para que no desconfigure el promedio
+
+    // Calcula el promedio de las muestras
+    samples[sampleIndex] = distance;
+    sampleIndex++;
+    if (sampleIndex > 10) {sampleIndex = 0;}
+
+    float sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += samples[i];
+    }
+    distance = sum / 10;
 
     // Actualiza los valores máximos y mínimos
-    if (distance > maxHeight)
-    {
-        maxHeight = distance;
-    }
-    if (distance < minHeight)
-    {
-        minHeight = distance;
-    }
+    if (distance > maxHeight) {maxHeight = distance;}
+    if (distance < minHeight) {minHeight = distance;}
 
     // Devuelve numeros entre 0 y 127 basados en la distancia
     int modWheel = (distance - minHeight) / (maxHeight - minHeight) * 127;
@@ -40,6 +70,14 @@ void loop()
     // Imprime la distancia en la consola serial
     Serial.println(modWheel);
 
+
+
+    // Normaliza los valores máximos y mínimos
+    maxHeight = maxHeight + 0.001*(distance - maxHeight); //negativo
+    minHeight = minHeight + 0.001*(distance - minHeight); //positivo
+
+    delay(10);
+
     // Espera un poco antes de la próxima medición
-    delay(10); // 10 ms, latencia típica de instrumentos MIDI
+     // 10 ms, latencia típica de instrumentos MIDI
 }
